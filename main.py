909b042f23a27bda690654a33ee76e3aa11e9e4f
Adrian Rodriguez <115055386+tomasrod19@users.noreply.github.com>
2024-08-21 13:41:26 -0700

    added main

---
+++ main.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QApplication
+from gui import VideoEditorGUI
+
+def main():
+    app = QApplication([])
+
+    window = VideoEditorGUI()
+    window.initUI()
+    window.show()
+
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
+    
