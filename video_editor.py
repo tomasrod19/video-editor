91167d005030d277a5b7e3e5daee6bb2d1cd27bd
Adrian Rodriguez <115055386+tomasrod19@users.noreply.github.com>
2024-08-23 22:41:38 -0700

    Added function to detect what parts of the video have silence

--- video_editor.py
+++ video_editor.py
@@ -1,6 +1,6 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 from pydub import AudioSegment
-from pydub.silence import detect_nonsilent
+from pydub.silence import detect_nonsilent, detect_silence
 import os
 
 videos = ["./videos/vod1.mp4","./videos/vod2.mp4","./videos/vod3.mp4"]
@@ -24,6 +24,41 @@ def concatenate_videos(*video_paths):
 
 
 
+def mark_silent_times(video_path):
+    video = VideoFileClip(video_path)
+    audio = video.audio
+
+    # Export audio to a temporary file
+    temp_audio_path = "temp_audio.wav"
+    audio.write_audiofile(temp_audio_path)
+
+    # Load the audio file with pydub
+    audio_segment = AudioSegment.from_file(temp_audio_path, format="wav")
+
+    # Detect silent segments
+    audio_segments = detect_silence(audio_segment, min_silence_len=1000)
+
+    print(audio_segments)
+
+    silent_times = []
+    for segment in audio_segments:
+        if segment[1] - segment[0] > 1000:
+            silent_times.append(segment)
+
+    # Clean up the temporary audio file
+    #os.remove(temp_audio_path)
+
+    return silent_times
+
+print(mark_silent_times(videos[0]))
+
+
+
+
+
+
+
+
 
 
 
@@ -38,3 +73,6 @@ else:
 
 concatenate_videos(videos[0], videos[1], videos[2])  # Concatenating multiple videos
 '''
+
+
+
