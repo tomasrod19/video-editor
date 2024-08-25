263d68a8803307dc5aaf2b8cfbb61dc72df62287
Adrian Rodriguez <115055386+tomasrod19@users.noreply.github.com>
2024-08-24 23:15:57 -0700

    trimmed silences from video and put clips togehter

--- video_editor.py
+++ video_editor.py
@@ -23,8 +23,7 @@ def concatenate_videos(*video_paths):
     final_video.write_videofile(final_video_path)
 
 
-
-def mark_silent_times(video_path):
+def mark_noisy_times(video_path):
     video = VideoFileClip(video_path)
     audio = video.audio
 
@@ -35,44 +34,48 @@ def mark_silent_times(video_path):
     # Load the audio file with pydub
     audio_segment = AudioSegment.from_file(temp_audio_path, format="wav")
 
-    # Detect silent segments
-    audio_segments = detect_silence(audio_segment, min_silence_len=1000)
+    # Detect non-silent segments
+    audio_segments = detect_nonsilent(audio_segment, min_silence_len=1000, silence_thresh=-30)
 
     print(audio_segments)
 
-    silent_times = []
+    noisy_times = []
     for segment in audio_segments:
         if segment[1] - segment[0] > 1000:
-            silent_times.append(segment)
+            noisy_times.append(segment)
 
     # Clean up the temporary audio file
-    #os.remove(temp_audio_path)
+    os.remove(temp_audio_path)
 
-    return silent_times
+    print(noisy_times)  # Print the noisy_times variable
 
-print(mark_silent_times(videos[0]))
-
-
+    return audio_segments
 
 
+'''Trimmed silences from the video'''
 
+times = mark_noisy_times(videos[0])
 
-
-
-
+times_seconds = [(start / 1000, end / 1000) for start, end in times]
 
 
 
-''' Testing the functions below
 if os.path.exists(videos[0]):
-    trimmed_video = trim_video(videos[0], 0, 4)
+    trimmed_video = trim_video(videos[0], times_seconds[0][0], times_seconds[0][1])
+    trimmed_video2 = trim_video(videos[0], times_seconds[1][0], times_seconds[1][1])
     trimmed_video_path = os.path.join(video_folder, "trimmed_video.mp4")
+    trimmed_video_path2 = os.path.join(video_folder, "trimmed_video2.mp4")
     trimmed_video.write_videofile(trimmed_video_path)
+    trimmed_video2.write_videofile(trimmed_video_path2)
 else:
     print("Video file not found.")
 
-concatenate_videos(videos[0], videos[1], videos[2])  # Concatenating multiple videos
-'''
+videos.append(trimmed_video_path)
+videos.append(trimmed_video_path2)
+
+
+
+concatenate_videos(videos[3],videos[4])
 
 
 
